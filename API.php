49576f8e33dc09ff5c629aa7b1669938d146d76f
Robin Hedlund <robin.live@live.se>
2021-05-06 17:09:17 +0200

    living dangerously so this is first commit

---
+++ API.php
@@ -0,0 +1,135 @@
+<?php
+
+class API {
+    
+
+    /**
+     * Static varibles to be used throughout the API class
+     */
+    public static $products = array();
+    public static $categories = array(
+        'mens clothing',
+        'jewelery',
+        'electronics',
+        'womens clothing'
+    );
+
+
+    /**
+     * Main class
+     * Try / Catch query validation through validate functions
+     * Setting products array to all products if query is NOT set
+     * Setting products array to CATEGORY and or SHOW limit if query IS set
+     * Printing array to user
+     */
+    public static function main(){
+        
+        $show = self::query('show');
+        $category = self::query('category');
+        
+        try{
+            !self::validateCategory($category);
+        } catch (exception $e) {
+            echo $e->getMessage();
+            exit();
+        }
+        
+        try{
+            !self::validateShow($show);
+        } catch (exception $e) {
+            echo $e->getMessage();
+            exit();
+        }
+            
+        self::$products = self::getProducts();
+        if($category) self::$products = self::getCategory($category);        
+        if($show) self::$products = self::getAmount($show);
+        
+        $data = json_encode(self::$products, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        echo $data;
+    }
+    
+
+    /**
+     * Fetching PHP array of all products 
+     * Returning array
+     */
+    public static function getProducts(){        
+        include("productsArray.php");
+        return $productsArray;
+    }
+
+
+    /**
+     * Saving initial products to a new array to randomize through shuffle
+     * Splicing array depending on value of show
+     * Returning the new array
+     */
+    public static function getAmount($show){
+        
+        $randomizedArray = self::$products;
+        
+        shuffle($randomizedArray);
+        
+        array_splice($randomizedArray, $show); 
+        
+        return $randomizedArray;
+    }
+
+
+    /**
+     * New array to save the values of selected category index
+     * Looping through the products to find corresponding categories
+     * Returning the new array  
+     */
+    public static function getCategory($category){
+
+        $selectedCategory = array();
+  
+        foreach(self::$products as $key => $product){
+            if($product['category'] === $category){
+                array_push($selectedCategory, $product);
+            }
+        }
+
+        return $selectedCategory;
+    }
+    
+
+    /**
+     * Checks if category is in query and tries to match it with the categories array
+     * Throws exception if not true
+     */
+    public static function validateCategory($category){
+        
+        if($category && !in_array($category, self::$categories)){
+            throw new exception("Category not found!");
+        }
+    }
+
+
+    /**
+     * Checks to see if show is a number
+     * Checks to see if between 1 and 20
+     * Throws exception if not true
+     */
+    public static function validateShow($show){
+        
+        if($show && !is_numeric($show)){
+            throw new exception("Must be a number!");
+        }
+        elseif($show && ($show < 1 || $show > 20)){
+            throw new exception("Must be a number between 1 and 20!");
+        }
+    }
+
+    /**
+     * Returns query string IF set
+     * Returns false if query is NOT set
+     */
+    public static function query($query){
+        return isset($_GET[$query]) ? filter_var($_GET[$query], FILTER_SANITIZE_STRING) : false;
+    }
+}
+
+?>
---
+++ productsArray.php
@@ -0,0 +1,165 @@
+
+<?php
+    $productsArray = array(
+        array(
+        "id"=> 1,
+        "title"=> "Black Bad Boy Boots",
+        "description"=> "Walk like a motherhugging king",
+        "image"=> "https://cdn.shopify.com/s/files/1/2521/5640/products/Luca_BlackLux_01_1800x1800.jpg?v=1566318524",
+        "price"=> 3800,
+        "category"=> "mens clothing"
+        ),
+        array(
+        "id"=> 2,
+        "title"=> "Bikeless Leather Jacket",
+        "description"=> "We all know you can't afford the bike",
+        "image"=> "https://assets.trouva.com/image/upload/w_690,f_auto,q_auto:eco,dpr_auto,c_pad,b_white/v1604063150/brand/5da6e14620dd0500038c09b0/14432544-de4a-4d36-a11e-1034c6b733df.jpg",
+        "price"=> 4500,
+        "category"=> "mens clothing"
+        ),
+        array(
+        "id"=> 3,
+        "title"=> "Knitted Gucci Sweater",
+        "description"=> "Nothing feels better than a Gucci sweater",
+        "image"=> "https://cdn-images.farfetch-contents.com/14/60/78/59/14607859_23127090_480.jpg",
+        "price"=> 7999,
+        "category"=> "mens clothing"
+        ),
+        array(
+        "id"=> 4,
+        "title"=> "Camo Beanie",
+        "description"=> "Camo (WHERE IS IT??) beanie from Carharttttttttt",
+        "image"=> "https://cdna.lystit.com/1040/1300/n/photos/nugnes1920/fc15e444/carhartt-Green-Hat-Man.jpeg",
+        "price"=> 599,
+        "category"=> "mens clothing"
+        ),
+        array(
+        "id"=> 5,
+        "title"=> "DB Backback",
+        "description"=> "Backpack from and for douchebags.",
+        "image"=> "https://douchebags.centracdn.net/client/dynamic/images/80_068992bb5a-explorer_black-full.jpg",
+        "price"=> 1999,
+        "category"=> "mens clothing"
+        ),
+        array(
+        "id"=> 6,
+        "title"=> "Fat Dollar Sign Gold Necklace",
+        "description"=> "Nice chains bro",
+        "image"=> "https://images-na.ssl-images-amazon.com/images/I/81H2B3uGTPL._AC_UL1500_.jpg",
+        "price"=> 100000,
+        "category"=> "jewelery"
+        ),
+        array(
+        "id"=> 7,
+        "title"=> "Engagement Ring",
+        "description"=> "Oh no now you're fucked forever",
+        "image"=> "https://i.pinimg.com/originals/aa/e0/27/aae027de9d0a8d7dda03fd52b8492c8e.jpg",
+        "price"=> 50000,
+        "category"=> "jewelery"
+        ),
+        array(
+        "id"=> 8,
+        "title"=> "Illuminati Ring",
+        "description"=> "So everyone can see you're in the new world order",
+        "image"=> "https://images-na.ssl-images-amazon.com/images/I/71JVtkLXtBL._SY500_.jpg",
+        "price"=> 0,
+        "category"=> "jewelery"
+        ),
+        array(
+        'id'=>  9,
+        "title"=> "Battery as Earring",
+        "description"=> "If you can't afford anything else",
+        "image"=> "https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_1.0,f_auto,h_337,q_auto,w_600/c_pad,h_337,w_600/F7082361-01?pgw=1",
+        "price"=> 10,
+        "category"=> "jewelery"
+        ),
+        array(
+        "id"=> 10,
+        "title"=> "Average Looking Nose Ring",
+        "description"=> "Show everyone how edgy you are",
+        "image"=> "https://images-na.ssl-images-amazon.com/images/I/41QZELkNwOL.jpg",
+        "price"=> 299,
+        "category"=> "jewelery"
+        ),
+        array(
+        "id"=> 11,
+        "title"=> "Apple Mac Pro",
+        "description"=> "An oversized cheese grater",
+        "image"=> "https://support.apple.com/library/content/dam/edam/applecare/images/en_US/social/2019-identify-mac-pro-social-card.jpg",
+        "price"=> 200000,
+        "category"=> "electronics"
+        ),
+        array(
+        "id"=> 12,
+        "title"=> "Samsung 100-inch QLED",
+        "description"=> "So you can watch everything bigger",
+        "image"=> "https://www.mansworldindia.com/wp-content/uploads/2018/09/The-Vu-100-TV-1-1024x675.jpg",
+        "price"=> 40000,
+        "category"=> "electronics"
+        ),
+        array(
+        "id"=> 13,
+        "title"=> "Xbox Series X",
+        "description"=> "Have fun not playing with all your friends since all of them have playstation",
+        "image"=> "https://www.techinn.com/f/13777/137776929/microsoft-xbox-series-x-1tb.jpg",
+        "price"=> 3000,
+        "category"=> "electronics"
+        ),
+        array(
+        "id"=> 14,
+        "title"=> "Robotic Lawn Mower",
+        "description"=> "We know we're all too lazy to to it ourselves",
+        "image"=> "https://i.pinimg.com/originals/45/e5/16/45e51631075b885e914828516a42c294.jpg",
+        "price"=> 4999,
+        "category"=> "electronics"
+        ),
+        array(
+        "id"=> 15,
+        "title"=> "Stolen Voi",
+        "description"=> "Only the best price for you my friend",
+        "image"=> "https://images.ohmyhosting.se/CmIW6qR66GXAhR34uZp0y10iJgk=/1080x928/smart/filters:quality(85)/https%3A%2F%2Fwww.voiscooters.com%2Fwp-content%2Fuploads%2F2019%2F09%2Fvoi2-1.jpg",
+        "price"=> 56.99,
+        "category"=> "electronics"
+        ),
+        array(
+        "id"=> 16,
+        "title"=> "80's Glasses",
+        "description"=> "Let's kick it like its 1985",
+        "image"=> "https://i.pinimg.com/originals/ed/b7/37/edb737927ee3e0a6414d8fdcdb813562.jpg",
+        "price"=> 299,
+        "category"=> "womens clothing"
+        ),
+        array(
+        "id"=> 17,
+        "title"=> "Hideous Boots",
+        "description"=> "Green boots(socks??) from Balenciaga",
+        "image"=> "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXdxn31dx9ORyvcZ0bjKwo_RjWl6B_PmxAYpWryPuqPPGpIATpryfFR2IqdVHdUiFebb89ZcSg&usqp=CAc",
+        "price"=> 7495,
+        "category"=> "womens clothing"
+        ),
+        array(
+        "id"=> 18,
+        "title"=> "Overpriced Floral Silk Dress",
+        "description"=> "Super spicy dress from Miu Miu",
+        "image"=> "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRosK-vLveMFjvRJahAO5WOu-Jq0IkIuPb6e-fQkZYsgo0r6IhuuVPvfGUhRLGHVv2jNIUtMW0C&usqp=CAc",
+        "price"=> 16999,
+        "category"=> "womens clothing"
+        ),
+        array(
+        "id"=> 19,
+        "title"=> "Cowboy Hat",
+        "description"=> "Release your inner Cindy Lauper",
+        "image"=> "https://i.pinimg.com/originals/fc/5a/ff/fc5affa15964640ca0e0c5cb964afab3.jpg",
+        "price"=> 1500,
+        "category"=> "womens clothing"
+        ),
+        array(
+        "id"=> 20,
+        "title"=> "Jurassic Park Mamasaurus Edition",
+        "description"=> "Don't mess with mamasaurus cause apparantly you'll get jurasskicked",
+        "image"=> "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0mVeRWXYuUNwwFA8wKfObDMTzgpcRV_MfVByl_TQguC9VMV4m8GaUcPIsDPayfEldtkknGmMz&usqp=CAc",
+        "price"=> 299,
+        "category"=> "womens clothing"
+        )
+    )
+?>
